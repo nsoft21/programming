312bd4812a973a88bdbb7707feadcf0acdfe8a40
nsoft21 <nickwerty5@gmail.com>
2020-12-27 19:11:21 +0300

    29

--- Practice/29/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
+++ Practice/29/C++/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,13 +1,11 @@
 ﻿#include <iostream>
 #include <map>
 #include <vector>
+#include <string>
+#include <ctime>
 
 typedef std::map<std::string, int> exams_map;
 typedef std::vector<std::string> new_vector;
-
-#include <iostream>
-#include <cmath>
-#include <vector>
 
 template<class K>
 using nvect = std::vector<K>;
@@ -70,13 +68,10 @@ template<class K>
 std::pair<nvect<K>, nmatr<K>> get_nums()
 {
     int len;
-    std::cout << "Введите число: ";
-    std::cin >> len;
     nmatr<K> matr;
     nvect<K> vect;
     nvect<K> temp;
 
-    std::cout << "Введите эл-ты массива через пробел: ";
     for (int i = 1; i <= len; i++)
     {
         K element;
@@ -100,24 +95,120 @@ void printv(nvect<K> arr) {
     std::cout << std::endl;
 }
 
-struct Student {
-	std::string name;
-	int group;
-	exams_map exams;
+struct Student
+{
+    std::map<std::string, int> exams_st;
+    std::string name_st;
+    int group_st;
+
+    Student(std::string name, int group,
+        int math, int phys, int hist, int prog)
+    {
+        if (group < 1 or group > 9)
+            throw std::invalid_argument("Error group: " + std::to_string(group));
+        if (math < 0 or math > 9 or phys < 0 or phys > 9 or hist < 0 or hist > 9
+            or prog < 0 or prog > 9)
+            throw std::invalid_argument("Error note: " +
+                std::to_string(math) + ", " + std::to_string(phys) + ", " + std::to_string(hist)
+                + ", " + std::to_string(prog));
+        name_st = name;
+        group_st = group;
+        exams_st = { {"math", math}, {"phys", phys}, {"hist", hist}, {"prog", prog} };
+    }
+
+    friend
+        std::ostream& operator << (std::ostream& out, const Student& student)
+    {
+        auto name_len = std::max(int(student.name_st.length()), 4);
+        std::string devider; for (int i = 0; i < name_len; ++i) devider += "─";
+        out << "┌─" << devider << "─┬───────┬──────┬──────┬──────┬──────┐\n"
+            << "│ Name " << std::string(name_len - 4, ' ')
+            << "│ Group │ Math │ Phys │ Hist │ Prog │\n"
+            << "├─" << devider << "─┼───────┼──────┼──────┼──────┼──────┤\n"
+            << "│ " << student.name_st << " │ " << student.group_st
+            << "     │ " << student.exams_st.at("math")
+            << "    │ " << student.exams_st.at("phys")
+            << "    │ " << student.exams_st.at("hist")
+            << "    │ " << student.exams_st.at("prog")
+            << "    │\n"
+            << "└─" << devider << "─┴───────┴──────┴──────┴──────┴──────┘\n";
+        return out;
+    }
+    bool operator < (const Student& other) const
+    {
+        return name_st < other.name_st;
+    }
+    bool operator > (const Student& other) const
+    {
+        return name_st > other.name_st;
+    }
+};
+
+std::ostream& operator << (std::ostream& out, const std::vector<Student>& student_vec)
+{
+    auto name_len = student_vec.at(0).name_st.length();
+    for (auto itr = student_vec.begin() + 1; itr != student_vec.end(); ++itr)
+    {
+        if (itr->name_st.length() > name_len) name_len = itr->name_st.length();
+    }
+    std::string devider; for (int i = 0; i < name_len; ++i) devider += "─";
+    out << "┌─" << devider << "─┬───────┬──────┬──────┬──────┬──────┐\n"
+        << "│ Name " << std::string(name_len - 4, ' ')
+        << "│ Group │ Math │ Phys │ Hist │ Prog │\n"
+        << "├─" << devider << "─┼───────┼──────┼──────┼──────┼──────┤";
+
+    for (auto& student : student_vec) {
+        out << "\n│ " << student.name_st
+            << std::string(name_len - student.name_st.length(), ' ')
+            << " │ " << student.group_st
+            << "     │ " << student.exams_st.at("math")
+            << "    │ " << student.exams_st.at("phys")
+            << "    │ " << student.exams_st.at("hist")
+            << "    │ " << student.exams_st.at("prog")
+            << "    │\n"
+            << "├─" << devider << "─┼───────┼──────┼──────┼──────┼──────┤";
+    }
+    out << "\r" << "└─" << devider << "─┴───────┴──────┴──────┴──────┴──────┘\n";
+    return out;
 };
 
 int main()
 {
-	setlocale(LC_ALL, "RUS");
-	
-	Student s[10] = {};
-	new_vector names = { "sd","sf","sg","sh","sj","sk","sl","sa","ss","sq" };
-	
-	for (int i = 0; i < 10; i++) {
-		s[i].name = names[i];
-		s[i].group = 1;
-		s[i].exams["math"] = 2;
-	}
-	
-	
+    setlocale(LC_ALL, "RUS");
+    std::srand(std::time(0));
+    std::vector<Student> students = {
+        Student("Петров А",     5, 2, 5, 4, 5),
+        Student("Щеглов Н",    5, 3, 3, 5, 3),
+        Student("Иванов С",     5, 4, 5, 4, 5),
+        Student("Поребин Г",    5, 2, 3, 5, 2),
+        Student("Белый А",      5, 3, 5, 4, 3),
+        Student("Григорьев П",       5, 4, 3, 5, 5),
+        Student("Лобанов Е",    5, 2, 5, 4, 2),
+        Student("Цукер М",      5, 3, 3, 5, 3),
+        Student("Путя Р",      5, 4, 5, 4, 5),
+        Student("Колобок В",  5, 2, 3, 5, 2),
+    };
+
+    auto has_failed = [](Student& student) -> bool {
+        for (auto& pair : student.exams_st)
+        {
+            if (pair.second == 2) return true;
+        }
+        return false;
+    };
+
+    std::vector<Student> failed;
+    for (auto& student : students)
+    {
+        if (has_failed(student)) failed.push_back(student);
+    }
+    if (failed.size() > 0)
+    {
+        std::cout << failed << std::endl;
+        std::cout << "Explusion\n" << failed.at(std::rand() % failed.size()) << std::endl;
+    }
+    else
+    {
+        std::cout << "Not found" << std::endl;
+    }
 }
